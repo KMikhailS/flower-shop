a8b481568b6e88b487d0344951603e917aab1dea
Mikhail Krivoshchapov <kmikhails@yandex.ru>
2025-12-10 22:20:30 +0500

    version up

--- app/src/components/SearchBar.tsx
+++ app/src/components/SearchBar.tsx
@@ -7,7 +7,7 @@ const SearchBar: React.FC = () => {
         <img src="/images/search.svg" alt="Search" className="w-6 h-6" />
         <input
           type="text"
-          placeholder="Поиск версия 0.56"
+          placeholder="Поиск версия 0.57"
           className="bg-transparent flex-1 outline-none font-inter text-base text-gray-medium placeholder:text-gray-medium"
         />
       </div>
